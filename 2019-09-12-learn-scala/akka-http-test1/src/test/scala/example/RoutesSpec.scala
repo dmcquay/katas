2a202a59e620306cba0b7cf3e3257be7b620bbfc
Dustin McQuay <dustin-mcquay@pluralsight.com>
2019-09-16 15:49:16 -0600

    test swapi endpoint

--- 2019-09-12-learn-scala/akka-http-test1/src/test/scala/example/RoutesSpec.scala
+++ 2019-09-12-learn-scala/akka-http-test1/src/test/scala/example/RoutesSpec.scala
@@ -1,14 +1,24 @@
+import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import org.scalatest.{Matchers, WordSpec}
-import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.testkit.{RouteTestTimeout, ScalatestRouteTest}
+import scala.concurrent.duration._
 
 class RoutesSpec extends WordSpec with Matchers with ScalatestRouteTest {
+  implicit def default(implicit system: ActorSystem) = RouteTestTimeout(5.seconds)
+
   "The service" should {
 
     "return a greeting for GET requests to the hello path" in {
       Get("/test/hello") ~> Routes.buildRoute ~> check {
         responseAs[String] shouldEqual "HELLO FUTURE WORLD"
+      }
+    }
+
+    "return luke skywalker's data for GET requests to the swapi path" in {
+      Get("/test/swapi") ~> Routes.buildRoute ~> check {
+        responseAs[String] shouldEqual "{\"name\":\"Luke Skywalker\",\"height\":\"172\",\"mass\":\"77\",\"hair_color\":\"blond\",\"skin_color\":\"fair\",\"eye_color\":\"blue\",\"birth_year\":\"19BBY\",\"gender\":\"male\",\"homeworld\":\"https://swapi.co/api/planets/1/\",\"films\":[\"https://swapi.co/api/films/2/\",\"https://swapi.co/api/films/6/\",\"https://swapi.co/api/films/3/\",\"https://swapi.co/api/films/1/\",\"https://swapi.co/api/films/7/\"],\"species\":[\"https://swapi.co/api/species/1/\"],\"vehicles\":[\"https://swapi.co/api/vehicles/14/\",\"https://swapi.co/api/vehicles/30/\"],\"starships\":[\"https://swapi.co/api/starships/12/\",\"https://swapi.co/api/starships/22/\"],\"created\":\"2014-12-09T13:50:51.644000Z\",\"edited\":\"2014-12-20T21:17:56.891000Z\",\"url\":\"https://swapi.co/api/people/1/\"}"
       }
     }
 
