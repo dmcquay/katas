9b0123f96b431ff4a9bb462d31de8bcc4537b4dc
Dustin McQuay <dustin-mcquay@pluralsight.com>
2019-10-11 11:19:40 -0600

    basic rate limiting working

--- 2019-10-11-rate-limiting/rate-limit.e2e.js
+++ 2019-10-11-rate-limiting/rate-limit.e2e.js
@@ -1,24 +1,34 @@
 const fetch = require('node-fetch')
 const {expect} = require('chai')
 
-it('should return OK 5 times', async () => {
-    for (let i = 0; i < 5; i++) {
+describe('rate limiting of /', () => {
+    before(async () => {
+        const response = await fetch('http://localhost:3000/reset-rate-limits')
+        expect(response.status).to.equal(200)
+        await response.text()
+    })
+
+    it('should return OK 5 times', async () => {
+        for (let i = 0; i < 5; i++) {
+            const response = await fetch('http://localhost:3000/')
+            expect(response.status).to.equal(200)
+            await response.text()
+        }
+    })
+
+    it('on the sixth try we should get a 429 indicating that we have been rate limited', async () => {
+        const response = await fetch('http://localhost:3000/')
+        expect(response.status).to.equal(429)
+        await response.text()
+    })
+
+    it('after waiting 2 seconds, we should get a 200 response again', async function() {
+        this.timeout(3000);
+
+        await new Promise(resolve => setTimeout(resolve, 2000))
+
         const response = await fetch('http://localhost:3000/')
         expect(response.status).to.equal(200)
         await response.text()
-    }
-})
-
-it('on the sixth try we should get a 429 indicating that we have been rate limited', async () => {
-    const response = await fetch('http://localhost:3000/')
-    expect(response.status).to.equal(429)
-    await response.text()
-})
-
-it('after waiting 10 seconds, we should get a 200 response again', async () => {
-    await new Promise(resolve => setTimeout(resolve, 10000))
-
-    const response = await fetch('http://localhost:3000/')
-    expect(response.status).to.equal(200)
-    await response.text()
+    })
 })
--- 2019-10-11-rate-limiting/server.js
+++ 2019-10-11-rate-limiting/server.js
@@ -2,8 +2,29 @@ const express = require('express')
 
 const app = express()
 
+let count = 0
+
+let windowInterval = undefined;
+function resetRateLimits() {
+    if (windowInterval) clearInterval(windowInterval)
+    count = 0
+    windowInterval = setInterval(() => count = 0, 2000)
+}
+resetRateLimits()
+
+app.get('/reset-rate-limits', (req, res) => {
+    resetRateLimits()
+    res.send()
+})
+
 app.get('/', (req, res) => {
-    res.send('hello world')
+    if (count >= 5) {
+        res.status(429)
+        res.send('Too Many Requests in the last 10 seconds')
+    } else {
+        res.send('hello world')
+        count++
+    }
 })
 
 app.listen('3000', () => { console.log('http://localhost:3000') })
