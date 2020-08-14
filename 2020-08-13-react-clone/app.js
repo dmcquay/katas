301639e95d8a030a06fb9e42134abff71dcc0cac
Dustin McQuay <dustin-mcquay@pluralsight.com>
2020-08-13 20:06:12 -0600

    basic support for components

--- 2020-08-13-react-clone/app.js
+++ 2020-08-13-react-clone/app.js
@@ -1,19 +1,29 @@
 const root = document.getElementById('root')
 
+function Clock() {
+    return new Date().toISOString()
+}
+
 function e(name, props, children) {
     return {name, props, children}
 }
 
-const tree = e('p', null, [e('p', null, 'hello world'), 'stuff'])
+const tree = e('p', null, [e('p', null, 'hello world'), 'stuff', e(Clock)])
 
 function renderToDOM(vNode, domNode) {
+    if (typeof vNode === 'string') {
+        domNode.append(document.createTextNode(vNode))
+        return
+    }
+
     const e = document.createElement(vNode.name)
     const children = vNode.children instanceof Array ? vNode.children : [vNode.children]
     for (const child of children) {
-        if (typeof child === 'string')
-            e.append(document.createTextNode(child))
-        else
+        if(typeof child.name === 'function') {
+            renderToDOM(child.name(child.props), e)
+        } else {
             renderToDOM(child, e)
+        }
     }
     domNode.append(e)
 }
