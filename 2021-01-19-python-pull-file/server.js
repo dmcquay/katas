f20297fbdac3dbb396f9abb5bc70002cd40e27f8
Dustin McQuay <dustin-mcquay@plurlsight.com>
2021-01-26 11:46:36 -0700

    print out body

--- 2021-01-19-python-pull-file/server.js
+++ 2021-01-19-python-pull-file/server.js
@@ -7,14 +7,21 @@ Joey,66
 Susan,33`
 
 const requestHandler = (req, res) => {
-        console.log({
-                url: req.url,
-                headers: req.headers
-        });
-        res.writeHead(200, 'OK', {
-                'Content-type': 'text/csv'
-        });
-        res.end(data);
+        let body = ''
+        req.on('data', data => {
+                body += data
+        })
+        req.on('end', () => {
+                console.log({
+                        url: req.url,
+                        headers: req.headers,
+                        body
+                });
+                res.writeHead(200, 'OK', {
+                        'Content-type': 'text/csv'
+                });
+                res.end(data);
+        })
 }
 
 const server = http.createServer(requestHandler);
