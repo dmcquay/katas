b6f747618f2554e2bcc3deac4cffc9fb75b34534
Dustin McQuay <dustin-mcquay@plurlsight.com>
2022-04-24 10:39:09 -0600

    incremental solver

--- 2022-04-23-battleship/solve.go
+++ 2022-04-23-battleship/solve.go
@@ -16,16 +16,19 @@ type Ship struct {
 	shortLabel rune
 	longLabel  string
 	cells      []*Cell
+	isSunk     bool
 }
 
-func buildGrid() [10][10]*Cell {
+type Grid *[10][10]*Cell
+
+func buildGrid() Grid {
 	var grid [10][10]*Cell
 	for row := 0; row < 10; row++ {
 		for col := 0; col < 10; col++ {
 			grid[row][col] = &Cell{}
 		}
 	}
-	return grid
+	return &grid
 }
 
 func buildShips() []*Ship {
@@ -42,12 +45,13 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	grid := buildGrid()
 	ships := buildShips()
-	placeShips(ships, &grid)
+	placeShips(ships, grid)
 	printGrid(grid)
 	printShips(ships)
+	solveIncrementally(grid, ships)
 }
 
-func isLocationEmpty(grid *[10][10]*Cell, shipLen int, isOnCol bool, randShort int, randFull int) bool {
+func isLocationEmpty(grid Grid, shipLen int, isOnCol bool, randShort int, randFull int) bool {
 	if isOnCol {
 		for r := randShort; r < shipLen+randShort; r++ {
 			cell := (*grid)[r][randFull]
@@ -66,13 +70,13 @@ func isLocationEmpty(grid *[10][10]*Cell, shipLen int, isOnCol bool, randShort i
 	return true
 }
 
-func placeShips(ships []*Ship, grid *[10][10]*Cell) {
+func placeShips(ships []*Ship, grid Grid) {
 	for i := 0; i < len(ships); i++ {
 		placeShip(ships[i], grid)
 	}
 }
 
-func placeShip(s *Ship, grid *[10][10]*Cell) {
+func placeShip(s *Ship, grid Grid) {
 	var isOnCol bool = rand.Intn(2) == 1
 	var randShort int = rand.Intn(10 - s.length)
 	var randFull int = rand.Intn(10)
@@ -98,7 +102,7 @@ func placeShip(s *Ship, grid *[10][10]*Cell) {
 	}
 }
 
-func printGrid(grid [10][10]*Cell) {
+func printGrid(grid Grid) {
 	for i := 0; i < len(grid); i++ {
 		printCells(grid[i])
 	}
@@ -143,4 +147,40 @@ func printShip(ship *Ship) {
 		}
 	}
 	fmt.Printf("%s (%d)\tPlaced: %t, Hits: %d/%d\n", ship.longLabel, ship.length, placed, hitCount, ship.length)
+}
+
+func allShipsAreSunk(ships []*Ship) bool {
+	for i := 0; i < len(ships); i++ {
+		if !ships[i].isSunk {
+			return false
+		}
+	}
+	return true
+}
+
+func solveIncrementally(grid Grid, ships []*Ship) {
+	shotCount := 0
+	for row := 0; row < 10; row++ {
+		for col := 0; col < 10; col++ {
+			cell := grid[row][col]
+			cell.attacked = true
+			shotCount++
+			if cell.ship != nil && !cell.ship.isSunk {
+				fmt.Println("Hit a ship")
+				cell.ship.isSunk = true
+				for i := 0; i < len(cell.ship.cells); i++ {
+					if !cell.ship.cells[i].attacked {
+						cell.ship.isSunk = false
+					}
+				}
+				if cell.ship.isSunk {
+					fmt.Println("Sunk a ship")
+				}
+			}
+			if allShipsAreSunk(ships) {
+				fmt.Printf("Solved in %d shots\n", shotCount)
+				return
+			}
+		}
+	}
 }
