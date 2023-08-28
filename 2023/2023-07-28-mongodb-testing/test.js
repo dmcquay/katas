e0cb0cdcacb41d39ab66c95c31248f572dc44657
Dustin McQuay <dustin.mcquay@boosterfuels.com>
2023-08-28 17:44:54 -0600

    make more useful as a reference

--- 2023/2023-07-28-mongodb-testing/test.js
+++ 2023/2023-07-28-mongodb-testing/test.js
@@ -9,8 +9,12 @@ const client3server4 = new mongodb3.MongoClient("mongodb://localhost:27004/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+client3server4.ObjectId = mongodb3.ObjectId;
+client3server4.IncompatibleObjectId = mongodb4.ObjectId;
 
 const client4server4 = new mongodb4.MongoClient("mongodb://localhost:27004/");
+client4server4.ObjectId = mongodb4.ObjectId;
+client4server4.IncompatibleObjectId = mongodb3.ObjectId;
 
 const clients = { client3server4, client4server4 };
 
@@ -32,7 +36,21 @@ describe("mongo", () => {
         await client.close();
       });
 
-      it("returnOriginal", async () => {
+      // it("DEPRECATED IN v3: returnOriginal = false", async () => {
+      //   const { insertedId } = await users.insertOne({
+      //     doNotText: true,
+      //   });
+      //   const u1 = await users
+      //     .findOneAndUpdate(
+      //       { _id: insertedId },
+      //       { $set: { doNotText: false } },
+      //       { returnOriginal: false }
+      //     )
+      //     .then((r) => r.value);
+      //   expect(u1.doNotText).to.be.false;
+      // });
+
+      it("GOOD: returnDocument = after", async () => {
         const { insertedId } = await users.insertOne({
           doNotText: true,
         });
@@ -41,60 +59,67 @@ describe("mongo", () => {
           .findOneAndUpdate(
             { _id: insertedId },
             { $set: { doNotText: false } },
-            {
-              //   returnOriginal: false
-              returnDocument: "after",
-            }
+            { returnDocument: "after" }
           )
           .then((r) => r.value);
 
         expect(u1.doNotText).to.be.false;
       });
 
-      it("find.countDocuments", async () => {
+      // it("DEPRECATED IN v4: cursor.count()", async () => {
+      //   const testProp = randInt();
+      //   await users.insertOne({ testProp });
+      //   await users.insertOne({ testProp });
+      //   await users.insertOne({ testProp: 1 });
+      //   const count = await users.find({ testProp }).count();
+      //   expect(count).to.eq(2);
+      // });
+
+      // it("DOESN'T WORK IN ANY VERSION: cursor.countDocuments()", async () => {
+      //   const testProp = randInt();
+      //   await users.insertOne({ testProp });
+      //   await users.insertOne({ testProp });
+      //   await users.insertOne({ testProp: 1 });
+      //   const count = await users.find({ testProp }).countDocuments();
+      //   expect(count).to.eq(2);
+      // });
+
+      it("GOOD: collection.countDocuments()", async () => {
         const testProp = randInt();
         await users.insertOne({ testProp });
         await users.insertOne({ testProp });
         await users.insertOne({ testProp: 1 });
-
-        // deprecated in >= 4.x
-        // const count = await users.find({ testProp }).count();
-
-        // this does not work. you must call countDocuments on a collection, not a cursor
-        // const count = await users.find({ testProp }).countDocuments();
-
         const count = await users.countDocuments({ testProp });
-
         expect(count).to.eq(2);
       });
 
-      it("ensureIndex once", async () => {
-        await users.ensureIndex(
-          { ensureIndexOnce: 1 },
-          { name: "ensureIndex_once" }
-        );
-      });
+      // it("DEPRECATED IN v3: ensureIndex once", async () => {
+      //   await users.ensureIndex(
+      //     { ensureIndexOnce: 1 },
+      //     { name: "ensureIndex_once" }
+      //   );
+      // });
 
-      it("ensureIndex duplicate", async () => {
-        await users.ensureIndex(
-          { ensureIndexDuplicate: 1 },
-          { name: "ensureIndex_duplicate" }
-        );
+      // it("DEPRECATED IN v3: ensureIndex duplicate", async () => {
+      //   await users.ensureIndex(
+      //     { ensureIndexDuplicate: 1 },
+      //     { name: "ensureIndex_duplicate" }
+      //   );
 
-        await users.ensureIndex(
-          { ensureIndexDuplicate: 1 },
-          { name: "ensureIndex_duplicate" }
-        );
-      });
+      //   await users.ensureIndex(
+      //     { ensureIndexDuplicate: 1 },
+      //     { name: "ensureIndex_duplicate" }
+      //   );
+      // });
 
-      it("createIndex once", async () => {
+      it("GOOD: createIndex once", async () => {
         await users.createIndex(
           { createIndexOnce: 1 },
           { name: "createIndex_once" }
         );
       });
 
-      it("createIndex duplicate", async () => {
+      it("GOOD: createIndex duplicate", async () => {
         await users.createIndex(
           { createIndexDuplicate: 1 },
           { name: "createIndex_duplicate" }
@@ -106,21 +131,21 @@ describe("mongo", () => {
         );
       });
 
-      it("fetching inserted object from response.ops", async () => {
-        const doc = { name: `Customer ${Math.random()}` };
-        const response = await users.insertOne(doc);
-        const [first] = response.ops;
-        expect(first._id).to.not.be.undefined;
-      });
+      // it("REMOVED IN v4: fetching inserted object from response.ops", async () => {
+      //   const doc = { name: `Customer ${Math.random()}` };
+      //   const response = await users.insertOne(doc);
+      //   const [first] = response.ops;
+      //   expect(first._id).to.not.be.undefined;
+      // });
 
-      it("getting insertedId for inserted object via response.insertedId", async () => {
+      it("GOOD: getting insertedId for inserted object via response.insertedId", async () => {
         const doc = { name: `Customer ${Math.random()}` };
         const response = await users.insertOne(doc);
         const { insertedId } = response;
         expect(insertedId).to.not.be.undefined;
       });
 
-      it("get inserted id by assuming mongo driver adds it to passed document", async () => {
+      it("GOOD: get inserted id by assuming mongo driver adds it to passed document", async () => {
         const doc = { name: `Customer ${Math.random()}` };
         expect(doc._id).to.be.undefined;
         const response = await users.insertOne(doc);
@@ -128,9 +153,19 @@ describe("mongo", () => {
         expect(doc._id).to.eq(insertedId);
       });
 
-      it("updates other ids", async () => {
+      // it("BAD: updates other ids", async () => {
+      //   const doc = {
+      //     otherId: new client.IncompatibleObjectId(),
+      //   };
+      //   const { insertedId } = await users.insertOne(doc);
+      //   const doc2 = await users.findOne({ _id: insertedId });
+      //   expect(doc.otherId.toHexString()).to.eql(doc2.otherId.toHexString());
+      //   expect(doc.otherId).to.eql(doc2.otherId);
+      // });
+
+      it("GOOD: use ObjectId from same version of mongo", async () => {
         const doc = {
-          otherId: new mongodb4.ObjectId(),
+          otherId: new client.ObjectId(),
         };
         const { insertedId } = await users.insertOne(doc);
         const doc2 = await users.findOne({ _id: insertedId });
