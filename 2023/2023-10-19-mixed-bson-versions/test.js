cbbfce27c688c27803fba0e1b1eebb4f783f7bdb
Dustin McQuay <dustin.mcquay@boosterfuels.com>
2023-10-19 17:33:46 -0600

    updated version of clone works

--- 2023/2023-10-19-mixed-bson-versions/test.js
+++ 2023/2023-10-19-mixed-bson-versions/test.js
@@ -1,4 +1,5 @@
-const clone = require('clone');
+const clone0 = require('clone0');
+const clone2 = require('clone2');
 const bson1 = require('bson1');
 const bson4 = require('bson4');
 
@@ -6,7 +7,10 @@ const id1 = new bson1.ObjectId();
 const id4 = new bson4.ObjectId();
 
 // works fine with bson1
-console.log(JSON.stringify(clone(id1)));
+console.log(JSON.stringify(clone0(id1)));
 
 // but this throws TypeError: Cannot read properties of undefined (reading 'toString')
-console.log(JSON.stringify(clone(id4)));
+// console.log(JSON.stringify(clone0(id4)));
+
+// updated version of clone works
+console.log(JSON.stringify(clone2(id4)));
