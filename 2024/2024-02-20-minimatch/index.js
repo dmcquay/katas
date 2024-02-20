c9eea31b910f0416ee84de7551a0eb496fc9b87b
Dustin McQuay <dustin.mcquay@boosterfuels.com>
2024-02-20 16:34:37 -0700

    add a few more docs

--- 2024/2024-02-20-minimatch/index.js
+++ 2024/2024-02-20-minimatch/index.js
@@ -1,10 +1,25 @@
 const { minimatch } = require("minimatch");
 
-/**
-matchBase
-If set, then patterns without slashes will be matched against the basename of the path if it contains
-slashes. For example, a?b would match the path /xyz/123/acb, but not /xyz/acb/123.
- */
+// Cypress Docs: https://docs.cypress.io/api/commands/intercept#Glob-Pattern-Matching-URLs
+// Minimatch Docs: https://github.com/isaacs/minimatch
+
+// matchBase (snippet from Minimatch Docs):
+// If set, then patterns without slashes will be matched against the basename of the path if it contains
+// slashes. For example, a?b would match the path /xyz/123/acb, but not /xyz/acb/123.
+
+// output (for quick reference without running the code):
+// path: /xyz/123/acb, pattern: /xy**b, result: false
+// path: /xyz/123/acb, pattern: /xy**, result: false
+// path: /xyz/123/acb, pattern: /xyz/**, result: true
+// path: /xyz/123/acb/asdf/one/two, pattern: /xyz/**, result: true
+// path: /xyz/123/acb/asdf/one/two, pattern: /xyz/**/two, result: true
+// path: /xyz/123/acb, pattern: **/acb, result: true
+// path: /xyz/123/acb, pattern: a?b, result: true
+// path: /xyz/123/acb, pattern: /a?b, result: false
+// path: /xyz/123/acb, pattern: 123/a?b, result: false
+// path: /xyz/123/acb, pattern: /xyz/123/a?b, result: true
+// path: https://example.com/xyz/123/acb, pattern: /xyz/123/a?b, result: false
+// path: /xyz/123/acb/asdf, pattern: a?b, result: false
 
 const patterns = [
   ["/xyz/123/acb", "/xy**b"],
