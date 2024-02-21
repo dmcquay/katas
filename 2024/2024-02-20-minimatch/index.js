14da7ad86ec23d2823d0595822d757cc460b39b2
Dustin McQuay <dustin.mcquay@boosterfuels.com>
2024-02-20 17:00:23 -0700

    add note about matching with and without host

--- 2024/2024-02-20-minimatch/index.js
+++ 2024/2024-02-20-minimatch/index.js
@@ -3,6 +3,15 @@ const { minimatch } = require("minimatch");
 // Cypress Docs: https://docs.cypress.io/api/commands/intercept#Glob-Pattern-Matching-URLs
 // Minimatch Docs: https://github.com/isaacs/minimatch
 
+// We can't tell this from minimatch directly, but cy.intercept seems to allow matching
+// with or without the "host" (protocol://hostname:port) part of the URL.
+// e.g. This example below fails to match:
+// path: https://example.com/xyz/123/acb, pattern: /xyz/123/a?b, result: false
+// But cy.intercept('/xyz/123/a?b') will match the URL above.
+// We're not sure how this is happening and can't find clarity in the docs, but
+// we're guessing that cy.intercept might be calling minimatch with the full URL and
+// also with the path part of the URL.
+
 // matchBase (snippet from Minimatch Docs):
 // If set, then patterns without slashes will be matched against the basename of the path if it contains
 // slashes. For example, a?b would match the path /xyz/123/acb, but not /xyz/acb/123.
