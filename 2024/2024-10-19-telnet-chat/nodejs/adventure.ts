1282b90b3c4a8ccd3a1cf911e2c26b265bae1b86
Dustin McQuay <dmcquay@gmail.com>
2024-11-09 12:10:19 -0700

    finish lukes dungeon

--- 2024/2024-10-19-telnet-chat/nodejs/adventure.ts
+++ 2024/2024-10-19-telnet-chat/nodejs/adventure.ts
@@ -495,8 +495,192 @@ const luke: World = {
       ]
     },
     building_room: {
-      initial: 'You are in the building room',
-      actions: []
+      initial: 'You are in a dark room',
+      actions: [
+        {
+          name: 'Search behind you',
+          response: {
+            message: 'You found a piece of wood',
+            acquireItem: 'wood1'
+          }
+        },
+        {
+          name: 'Search in front you',
+          response: {
+            message: 'You found a piece of wood',
+            acquireItem: 'wood2'
+          }
+        },
+        {
+          name: 'Search above you',
+          response: {
+            message: 'You found a piece of wood attached to the ceiling. You are able to break it loose.',
+            acquireItem: 'wood3'
+          }
+        },
+        {
+          name: 'Build a key',
+          required: {
+            items: ['wood1', 'wood2', 'wood3'],
+            message: `You don't have anything with which to build a key`
+          },
+          response: {
+            message: 'You are very talented. You are able to build a key somehow from the three pieces of wood with your bare hands.',
+            acquireItem: 'build room wood key'
+          }
+        },
+        {
+          name: 'Unlock the door',
+          required: {
+            items: ['build room wood key'],
+            message: 'The door is locked'
+          },
+          response: {
+            message: 'You unlocked the door',
+            find: ['unlocked door']
+          }
+        },
+        {
+          name: 'Go through the door',
+          found: ['unlocked door'],
+          response: {
+            message: 'You leave the dark room through the door',
+            moveToRoom: 'main_hall'
+          }
+        }
+      ]
+    },
+    main_hall: {
+      initial: 'You are in the main hall. At the end of the hall is a locked door. There is a bomb just sitting on the floor. Part way down the hall is a small room with no door.',
+      actions: [
+        {
+          name: 'Unlock the door',
+          required: {
+            items: ['item does not exist'],
+            message: `You don't have a key that fits`
+          },
+          response: {
+            message: `THIS WILL NEVER BE TRIGGERED`
+          }
+        },
+        {
+          name: 'Explode the door',
+          required: {
+            items: ['matches'],
+            message: `How can we make the bomb explode? It seems to have a fuse...`
+          },
+          response: {
+            message: 'The door is blown up. You can walk through the rubble.',
+            moveToRoom: 'camera_hall'
+          }
+        },
+        {
+          name: 'Look in small room',
+          response: {
+            message: 'You found matches!',
+            acquireItem: 'matches'
+          }
+        }
+      ]
+    },
+    camera_hall: {
+      initial: `You are in another hallway. There are cameras, but you are behind them so they can't see you. There is a key hanging from a hook in the ceiling for some reason. There is a door at the end of the hall.`,
+      actions: [
+        {
+          name: 'Grab key hanging from ceiling',
+          response: {
+            acquireItem: 'camera hall key',
+            message: `You acquired a key`
+          }
+        },
+        {
+          name: 'Unlock the door',
+          required: {
+            items: ['camera hall key'],
+            message: `The door is locked.`
+          },
+          response: {
+            find: ['unlocked door'],
+            message: 'You unlocked the door'
+          }
+        },
+        {
+          name: 'Go through the door',
+          found: ['unlocked door'],
+          response: {
+            message: 'You go through the door',
+            moveToRoom: 'pre_boss_room'
+          }
+        }
+      ]
+    },
+    pre_boss_room: {
+      initial: 'This room is completely empty. But there is a little room on the side. A little monster starts attacking you. He is called the nibbler. There is a door with a sign above it that says "Boss Room".',
+      actions: [
+        {
+          name: 'Fight the nibbler',
+          response: {
+            message: 'Nibbler is dead',
+            find: ['dead nibbler']
+          }
+        },
+        {
+          name: 'Look in small room',
+          found: ['dead nibbler'],
+          response: {
+            message: 'You found a sword! It is actually a shape-shifting device that can turn into a sword, knife or scissors.',
+            acquireItem: 'sword'
+          }
+        },
+        {
+          name: 'Go through door to Boss Room',
+          response: {
+            moveToRoom: 'boss_room',
+            message: 'You proceed to the Boss Room'
+          }
+        }
+      ]
+    },
+    boss_room: {
+      initial: `There is a huge monster. It notices you and starts to attack.`,
+      actions: [
+        {
+          name: 'Hit monster with your fist',
+          response: {
+            message: 'Your fist is no match for this monster.'
+          }
+        },
+        {
+          name: 'Slice the monster',
+          required: {
+            items: ['sword'],
+            message: `You slice the monster with your hand. The monster is not phased.`
+          },
+          response: {
+            message: 'You slayed the monster.',
+            find: ['door to last room']
+          }
+        },
+        {
+          name: 'Go to next room',
+          found: ['door to last room'],
+          response: {
+            moveToRoom: 'last_room',
+            message: 'You go to the next room'
+          }
+        }
+      ]
+    },
+    last_room: {
+      initial: 'You are in the last room. You find a diamond. Go through the exit. You are now a free person and a diamon richer!',
+      actions: [
+        {
+          name: 'Exit',
+          response: {
+            message: 'Game over. You win. The end.'
+          }
+        }
+      ]
     }
   },
 };
